ac62f9081195967334113fdc1ce92f91f924de89
Ali El-Sayed Ali <58270273+Akumo961@users.noreply.github.com>
2026-08-26 18:11:14 -0400

    fix(db): use standard async Alembic migration transaction

--- alembic/env.py
+++ alembic/env.py
@@ -7,9 +7,9 @@ from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from dataguard.database.base import Base
-from dataguard.database import models  # noqa: F401
 from dataguard.core.config import get_settings
+from dataguard.database import models  # noqa: F401
+from dataguard.database.base import Base
 
 config = context.config
 settings = get_settings()
@@ -25,12 +25,16 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
+def do_run_migrations(connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
 async def run_async_migrations() -> None:
     connectable = async_engine_from_config(config.get_section(config.config_ini_section, {}), prefix="sqlalchemy.", poolclass=pool.NullPool)
     async with connectable.connect() as connection:
-        await connection.run_sync(lambda sync_conn: context.configure(connection=sync_conn, target_metadata=target_metadata))
-        async with connection.begin():
-            await connection.run_sync(lambda _: context.run_migrations())
+        await connection.run_sync(do_run_migrations)
     await connectable.dispose()
 
 
