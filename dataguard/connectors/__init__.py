b8bcc2566f303b8c4e0a0c17b133ebc98e423b83
Ali El-Sayed Ali <58270273+Akumo961@users.noreply.github.com>
2026-09-01 20:42:15 -0400

    feat: add enterprise connector contracts

---
+++ dataguard/connectors/__init__.py
@@ -0,0 +1,5 @@
+"""Provider-neutral enterprise connector contracts."""
+
+from dataguard.connectors.base import Connector, ConnectorObject, ConnectorType
+
+__all__ = ["Connector", "ConnectorObject", "ConnectorType"]
